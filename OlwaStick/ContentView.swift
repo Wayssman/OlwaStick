1882c9ba7beb35653b55c6d690f482df3b5b2315
Alexandr <AlexandrZhelanov@gmail.com>
2021-08-09 20:38:39 +0300

    Initial Commit

---
+++ OlwaStick/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OlwaStick
+//
+//  Created by Желанов Александр Валентинович on 09.08.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OlwaStick/OlwaStickApp.swift
@@ -0,0 +1,17 @@
+//
+//  OlwaStickApp.swift
+//  OlwaStick
+//
+//  Created by Желанов Александр Валентинович on 09.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct OlwaStickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
